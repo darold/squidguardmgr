acce5c51fe45abf079bb84576b3b539b6c44adcc
Tatsuya-Nonogaki <winfield@support.email.ne.jp>
2013-11-17 03:07:47 +0900

    Update configure.sh

--- contrib/configure.sh
+++ contrib/configure.sh
@@ -9,5 +9,6 @@ perl Makefile.PL \
  BASEURL=/squidguardmgr \
  SQUIDUSR=squid SQUIDGRP=squid
 
-# Make squidguardmgr httpd process's DocumentRoot.
+# Make squidguardmgr httpd process's DocumentRoot and log directory.
 sudo mkdir -p /var/www/squid-gui
+sudo mkdir -p /var/log/httpd/squidguardmgr
